db182be5434e49f0a5ef25b2f91857ef6bf55bd2
brandon9826 <brandonl2426@gmail.com>
2022-03-27 11:51:48 -0500

    first commit

--- move.js
+++ move.js
@@ -5,8 +5,56 @@ function move(element) {
         element.style.left = left + 'px'
         element.style.bottom = bottom + 'px'
     }
+    function moveWithArrowKeys (left, bottom, callback) {
+    let direction= null
+    let x = left;
+    let y = bottom;
+    function moveCharacter () {
+    
+    document.addEventListener('keydown', function(e) {
+        if(e.repeat) return;
+        if(e.key === 'ArrowLeft') {
+            direction = 'west'
+        }
+        if(e.key === 'ArrowUp') {
+            direction = 'north'
+        }
+        if(e.key === 'ArrowRight') {
+            direction = 'east'
+        }
+        if(e.key === 'ArrowDown') {
+            direction = 'south'
+        }
+        callback(direction)
+        
+    })
+    
+    document.addEventListener('keyup', function(e) {
+        direction = null
+        callback(direction)
+    })
+    if(direction === 'west') {  
+        x = x - 1
+    }
+    if(direction === 'north') { 
+        y = y + 1
+    }
+    if(direction === 'east') {
+        x = x + 1
+    }
+    if(direction === 'south') {
+        y = y - 1
+    }
+    element.style.left = x + 'px'
+    element.style.bottom = y + 'px'
+}
 
+
+setInterval(moveCharacter,1)
+    }
     return {
-        to: moveToCoordinates
+        to: moveToCoordinates,
+        withArrowKeys: moveWithArrowKeys
     }
 }
+
--- newImage.js
+++ newImage.js
@@ -2,5 +2,6 @@ function newImage(url){
     let image = document.createElement('img')
     image.src = url
     document.body.append(image)
+   
     return image
 }
